56c60595ec6d8c32b2bec6c32198705bdaf9e3b4
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2018-02-25 18:14:34 +0300

    Some refactoring and JAVADOC is added

--- src/main/java/page_objects/native_app/AddContactForm.java
+++ src/main/java/page_objects/native_app/AddContactForm.java
@@ -1,55 +1,79 @@
 package page_objects.native_app;
 
 import entities.nativeApp.Contact;
-import enums.CONTACT_MANAGER_CONSTATNS;
-import io.appium.java_client.pagefactory.AndroidBy;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.By;
+import io.appium.java_client.pagefactory.iOSFindBy;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import utils.DriverBundle;
 
-import java.sql.Driver;
-
-import static enums.CONTACT_MANAGER_CONSTATNS.*;
+import static enums.CONTACT_MANAGER_CONSTANTS.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class AddContactForm extends BasePageNative {
 
+    @iOSFindBy(id = "com.example.android.contactmanager:id/contactNameEditText")
     @AndroidFindBy(id = "com.example.android.contactmanager:id/contactNameEditText")
     private WebElement nameTextField;
 
+    @iOSFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Name']")
     @AndroidFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Name']")
     private WebElement nameTitle;
 
+    @iOSFindBy(id = "com.example.android.contactmanager:id/contactPhoneEditText")
     @AndroidFindBy(id = "com.example.android.contactmanager:id/contactPhoneEditText")
     private WebElement phoneTextField;
 
+    @iOSFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Phone']")
     @AndroidFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Phone']")
     private WebElement phoneTitle;
 
+    @iOSFindBy(id = "com.example.android.contactmanager:id/contactEmailEditText")
     @AndroidFindBy(id = "com.example.android.contactmanager:id/contactEmailEditText")
     private WebElement emailTextField;
 
+    @iOSFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Email']")
     @AndroidFindBy(xpath = "//android.widget.TextView[@content-desc='Contact Email']")
     private WebElement emailTitle;
 
+    @iOSFindBy(xpath = "//android.widget.Button[@content-desc='Save']")
     @AndroidFindBy(xpath = "//android.widget.Button[@content-desc='Save']")
     private WebElement saveButton;
 
 
+    /**
+     *  A method to send a String value using system keyboard to target WebElement
+     *  @param text - String value to send
+     *  @param target - target element to send keys using system keyboard
+     * */
     public void sendKeysViaKeyboard(String text, WebElement target) {
         target.click();
         DriverBundle.getDriver().getKeyboard().sendKeys(text);
         assertEquals(target.getText(), text, target.toString() + " element's content doesn't match expected values");
     }
 
+
+    /**
+     * Checks label element according to following criteria:
+     *  - Element is displayed
+     *  - Element is enabled for usage
+     *  - Element's content is equal to expected String
+     *  @param label - element being checked
+     *  @param expected - String value expected to be in element's text block
+     * */
     private void checkLabel(WebElement label, String expected) {
         assertTrue(label.isDisplayed(), "label with expected value [" + expected + "] is not displayed");
         assertEquals(label.getText(), expected, "label content does not match ");
     }
 
+    /**
+     * Checks textFiled element according to following criteria:
+     *  - Element is displayed
+     *  - Element is enabled for usage
+     *  - String sent using system keyboard is contained within element's text block
+     *  @param textField - element being checked
+     *  @param value - String value to input using system keyboard
+     * */
     private void checkTextField(WebElement textField, String value) {
         assertTrue(textField.isDisplayed(), "text field with expected value [" + value + "] is not displayed");
         assertTrue(textField.isEnabled(), "text field with expected value [" + value + "] is not enabled");
--- src/main/java/page_objects/native_app/AllContactsForm.java
+++ src/main/java/page_objects/native_app/AllContactsForm.java
@@ -1,5 +1,6 @@
 package page_objects.native_app;
 
+import enums.CONTACT_MANAGER_CONSTANTS;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 
@@ -11,10 +12,16 @@ public class AllContactsForm extends BasePageNative {
     @AndroidFindBy(xpath = "//android.widget.Button[@content-desc='Add Contact']")
     private WebElement addContactButton;
 
+    /**
+     * Checks page elements according to following criteria:
+     *  - 'Add Contact' button is displayed
+     *  - 'Add Contact' button is enabled for usage
+     *  - 'Add Contact' button text is valid
+     * */
     public void checkAddButton(){
         assertTrue(addContactButton.isDisplayed(), "'Add contact' button is not displayed");
         assertTrue(addContactButton.isEnabled(), "'Add contact' button is not enabled");
-        assertEquals(addContactButton.getText(), "Add Contact",
+        assertEquals(addContactButton.getText(), CONTACT_MANAGER_CONSTANTS.ADDBTN.getText(),
                 "'Add contact' button content does not match expected value");
     }
 
